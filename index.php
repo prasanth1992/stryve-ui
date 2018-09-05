35dbc4ad4a5350b299dddda4663c09ce3260bb05
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2018-09-05 16:38:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With');
+header('Access-Control-Allow-Methods: GET, PUT, POST');
+header( 'Location: /index.html' ) ;  
+?>
